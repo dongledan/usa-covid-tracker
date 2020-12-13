13ed363ca661d456ef7d6443959f056a26040f39
Danny Li <dongledan@yahoo.com>
2020-12-13 18:06:19 -0500

    navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import logo from '../images/logo.svg';
 import './App.css';
+
+import Navbar from '../components/Navbar';
 import Home from '../components/Home';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
