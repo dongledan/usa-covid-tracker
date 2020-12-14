1bb582d88cb058bef5b60e46477dbc4def6461f4
Danny Li <dongledan@yahoo.com>
2020-12-14 06:45:19 -0500

    google analytics

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
+import React,{useEffect} from "react";
+import ReactGA from "react-ga"; 
+
 import Header from '../components/Header';
 import Home from '../components/Home';
 import Footer from '../components/Footer';
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-7REMPM2NXY');
+    ReactGA.pageview(window.location.pathname);
+  })
   return (
     <div 
       style={{
