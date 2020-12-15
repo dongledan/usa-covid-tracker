722052ddcd806d7e5adbc48b9e1a6b1027f09646
Danny Li <dongledan@yahoo.com>
2020-12-15 17:47:38 -0500

    added google analytics

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
+import React,{useEffect} from "react";
+import ReactGA from "react-ga";
+
 import Header from '../components/Header';
 import Home from '../components/Home';
 import Footer from '../components/Footer';
 
 function App() {
- 
+  useEffect(() => {
+    ReactGA.initialize(process.env.REACT_APP_GOOGLE_UA_ID);
+    ReactGA.pageview(window.location.pathname);
+  })
   return (
     <div 
       style={{
